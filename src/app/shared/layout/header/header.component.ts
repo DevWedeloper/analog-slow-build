ae0f21cb8fbee9cdd44f81eeefb0f60e51ee734c
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 01:14:42 +0800

    attempt to pinpoint what causes slow build time

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AccountComponent } from './account/account.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { HeaderDarkModeComponent } from './header-dark-mode.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AccountComponent],
+  imports: [
+    CategoriesComponent,
+    ShoppingCartComponent,
+    HeaderDarkModeComponent,
+    AccountComponent,
+  ],
   template: `
+    <app-categories />
+    <app-shopping-cart />
+    <app-dark-mode />
     <app-account />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
