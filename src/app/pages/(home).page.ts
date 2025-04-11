b12091d2cb0f669e0de5010c8913090c3a95aeb2
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 01:59:09 +0800

    attempt to pinpoint what causes slow build time

--- src/app/pages/(home).page.ts
+++ src/app/pages/(home).page.ts
@@ -1,13 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { HeaderComponent } from '../shared/layout/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
-  template: `
-    <app-header />
-  `,
+  template: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomePageComponent {}
