11f68c45d8724d9ec8e754587f8f8277eb845bf5
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 01:10:58 +0800

    attempt to pinpoint what causes slow build time

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,56 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AccountComponent } from './account/account.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { HeaderDarkModeComponent } from './header-dark-mode.component';
-import { HomeButtonComponent } from './home-button.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    HomeButtonComponent,
-    CategoriesComponent,
-    SearchBarComponent,
-    ShoppingCartComponent,
-    HeaderDarkModeComponent,
-    AccountComponent,
-  ],
-  host: {
-    class:
-      'sticky inset-0 z-10 block min-h-14 border-b border-border bg-background p-2',
-  },
+  imports: [AccountComponent],
   template: `
-    <nav>
-      <div class="hidden justify-between md:flex">
-        <div class="flex gap-2">
-          <app-home-button />
-          <app-categories />
-        </div>
-        <app-search-bar />
-        <div class="flex gap-2">
-          <app-shopping-cart />
-          <app-dark-mode />
-          <app-account />
-        </div>
-      </div>
-
-      <div class="flex flex-col gap-2 md:hidden">
-        <div class="flex justify-between">
-          <div class="flex gap-2">
-            <app-home-button />
-            <app-categories />
-          </div>
-          <div class="flex gap-2">
-            <app-shopping-cart />
-            <app-dark-mode />
-            <app-account />
-          </div>
-        </div>
-        <app-search-bar />
-      </div>
-    </nav>
+    <app-account />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
