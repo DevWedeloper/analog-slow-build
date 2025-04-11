11bf1105cf62f898701003fec1bf01e049d24cb3
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 02:42:21 +0800

    attempt to pinpoint what causes slow build time

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-export const client = postgres(import.meta.env['DATABASE_URL']);
+const client = postgres(import.meta.env['DATABASE_URL'], {
+  idle_timeout: 1000,
+  connect_timeout: 1000,
+});
 export const db = drizzle(client);
--- src/server/trpc/routers/index.ts
+++ src/server/trpc/routers/index.ts
@@ -1,14 +1,11 @@
-import { client, db } from 'src/db';
+import { db } from 'src/db';
 import { categories } from 'src/db/schema';
 import { publicProcedure, router } from '../trpc';
 
 export const appRouter = router({
   test: router({
     test: publicProcedure.query(
-      async () => {
-        const data = await db.select().from(categories).limit(1);
-        return data;
-      },
+      async () => await db.select().from(categories).limit(1),
     ),
   }),
 });
