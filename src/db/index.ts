32da13449bc30830b99d924bbc15b74d38f9557f
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 02:54:44 +0800

    attempt to pinpoint what causes slow build time

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 const client = postgres(import.meta.env['DATABASE_URL'], {
-  idle_timeout: 5,
-  ssl: true,
+  idle_timeout: 20,
 });
 export const db = drizzle(client);
