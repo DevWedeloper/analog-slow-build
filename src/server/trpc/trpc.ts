666662bf1430f6044f3ca9c9e6b5f935c6b78ae7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 02:20:16 +0800

    attempt to pinpoint what causes slow build time

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -1,5 +1,4 @@
-import { initTRPC, TRPCError } from '@trpc/server';
-import { createClient } from 'src/supabase/server';
+import { initTRPC } from '@trpc/server';
 import { SuperJSON } from 'superjson';
 import { Context } from './context';
 
@@ -10,23 +9,3 @@ const t = initTRPC.context<Context>().create({
 export const router = t.router;
 export const middleware = t.middleware;
 export const publicProcedure = t.procedure;
-export const protectedProcedure = t.procedure.use(
-  async ({ ctx: { event }, next }) => {
-    const supabase = createClient(event);
-
-    const { data, error } = await supabase.auth.getUser();
-
-    if (error) {
-      throw new TRPCError({
-        code: 'UNAUTHORIZED',
-        message: 'User must be authenticated',
-      });
-    }
-
-    return next({
-      ctx: {
-        user: data.user,
-      },
-    });
-  },
-);
