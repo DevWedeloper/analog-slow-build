4d747207a9314492ff964c1db6402e2c6e858da7
DevWedeloper <vicnathangabrielle@gmail.com>
2025-04-12 01:06:09 +0800

    attempt to pinpoint what causes slow build time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HlmToasterComponent } from '@spartan-ng/ui-sonner-helm';
+import { AppService } from './shared/data-access/app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmToasterComponent],
   template: `
     <router-outlet />
+    <hlm-toaster />
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  private appService = inject(AppService);
+
+  constructor() {
+    this.appService.checkIfRedirectedFromMagicLink();
+  }
+}
